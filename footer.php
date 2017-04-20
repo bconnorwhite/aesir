d49abff26c1d5a01b3100c9ee07eba0ce70ea5f0
ConnorWhite <connor.bcw@gmail.com>
2017-04-19 20:42:08 -0500

    Thick Footer

--- footer.php
+++ footer.php
@@ -1,13 +1,29 @@
 			<footer>
+				<h2>Connor <strong>White</strong></h2>
+				<hr />
 				<div class='wrap'>
-					<div class='social'>
-						<a href="http://www.linkedin.com/in/bconnorwhite" alt="LinkedIn">
-							<i class="fa fa-linkedin-square fa-2x" aria-hidden="true"></i>
-						</a>
-						<a href="mailto:connor@connorwhite.org" alt='Email'>
-							<i class="fa fa-envelope fa-2x" aria-hidden="true"></i>
-						</a>
+					<div class="social">
+						<ul>
+							<li>
+								<a href="mailto:connor@connorwhite.org" alt='Email'>
+									<i class="fa fa-envelope fa" aria-hidden="true"></i>connor [at] connorwhite.org
+								</a>
+							</li>
+							<li>
+								<a href="tel:19794922657" alt='Phone'>
+									<i class="fa fa-phone fa" aria-hidden="true"></i>1.979.492.2657
+								</a>
+							</li>
+							<li>
+								<a href="http://www.linkedin.com/in/bconnorwhite" alt="LinkedIn">
+									<i class="fa fa-linkedin-square fa" aria-hidden="true"></i>LinkedIn
+								</a>
+							</li>
+						</ul>
 					</div>
+				</div>
+				<div class="copyright">
+					Copyright © <?php echo date("Y"); ?> - All rights reserved.
 				</div>
 				<script async src="https://use.fontawesome.com/1b52717aea.js"></script>
 				<script async type="text/javascript" src="<?php bloginfo('template_url'); ?>/scripts/menu.js"></script>
