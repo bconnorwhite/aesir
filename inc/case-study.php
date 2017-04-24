237a1759734025ed9957fe40cc00596f5f9fb2e3
ConnorWhite <connor.bcw@gmail.com>
2017-04-24 16:07:33 -0500

    Porfolio images as img not background

--- inc/case-study.php
+++ inc/case-study.php
@@ -1,4 +1,5 @@
-<div class="case-study" style="background: url(<?php the_post_thumbnail_url() ?>);">
+<div class="case-study">
+  <img alt="<?php echo strip_tags(get_the_title()); ?>" src="<?php the_post_thumbnail_url() ?>" />
   <div class="shade"></div>
   <h2><?php the_title(); ?></h2>
 </div>
